0782fd1780a05925f1b860dc178fd6bfd4ecb7b4
primChareka <primrose_chareka@hotmail.com>
2018-09-24 09:58:44 -0400

    added proper comments to question 1.02

--- java_solutions/src/StringQuestions/Q1_02.java
+++ java_solutions/src/StringQuestions/Q1_02.java
@@ -1,23 +1,40 @@
-package StringQuestions;
+/**
+ * Cracking-The-Coding-Interview
+ * Chapter 1 - Arrays and Strings
+ * Q1_02.java
+ * 
+ * Question:
+ * 
+ * Given two strings, write a method to decide if one is the permutation of other.
+ * 
+ */
 
+package StringQuestions;
 import java.util.ArrayList;
 
 public class Q1_02 {
-
-	public static boolean Permutation(String first, String second) {
+	/*
+	 * Method to check if two strings are permutation 
+	 * 
+	 * O(n^2) in case of string are palindrome (must search to end of values array each time)
+	 * 
+	 * @param 
+	 * @param str2
+	 * @return {@link boolean}
+	 */
+	public static boolean Permutation(String string1, String string2) {
 		
-		if(first==null||second==null) {
+		if(string1==null||string2==null||string1==""||string2 == "") {
 			return false;
-		}else if(first.length()!=second.length()) {
+		}else if(string1.length()!=string2.length()) {
 			return false;
-		}else {
-			
-			ArrayList values = new ArrayList<Character>();
-			for(char i : first.toCharArray()) {
+		}else {		
+			ArrayList<Character> values = new ArrayList<Character>();
+			for(char i : string1.toCharArray()) {
 				values.add(i);
 			}
 			
-			for(char i : second.toCharArray()) {
+			for(char i : string2.toCharArray()) {
 				if(values.contains(i)) {
 					values.remove(Character.valueOf(i));
 				}else {
@@ -29,9 +46,25 @@ public class Q1_02 {
 	}
 
 	public static void main(String[] args) {
-		String first = "Hey";
-		String second ="euH";
-		System.out.println(Permutation(first,second));
+		String string1 = "Hey";
+		String string2 ="euH";
+		System.out.println(Permutation(string1,string2));
+		
+		string1 = "Hey";
+		string2 ="eyH";
+		System.out.println(Permutation(string1,string2));
+		
+		string1 = "Hey";
+		string2 ="eyH1";
+		System.out.println(Permutation(string1,string2));
+		
+		string1 = null;
+		string2 = null;
+		System.out.println(Permutation(string1,string2));
+		
+		string1 = "";
+		string2 = "";
+		System.out.println(Permutation(string1,string2));
 
 	}
 
